e92f22b7513a8d97650602c10ac5078d1232c94e
Hossam Hatem <hosam.hatem222@gmail.com>
2025-03-02 17:25:01 +0200

    remove builded version & update .gitignore file

--- dist/modules/User/user.validation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.update = void 0;
-const validation_middleware_1 = require("../../middleware/validation.middleware");
-const zod_1 = require("zod");
-exports.update = zod_1.z.object({
-    name: validation_middleware_1.generalFields.userName.optional(),
-    phone: validation_middleware_1.generalFields.phone.optional(),
-}).strict();
